1fdb5b3a75c0109b13665336ea63d405e4abe89f
zhangwenjun26 <zhangwenjun9@jd.com>
2025-01-24 15:38:52 +0800

    更新配置

--- bin/stop.sh
+++ bin/stop.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-process_name=main_manage
+process_name=dbgpt_server
 BASE_DIR=$(cd `dirname $0`;cd ..;pwd)
 PID_FILE="$BASE_DIR/pid/service.pid"
 
@@ -9,20 +9,6 @@ child_pids=$(pgrep -P $main_pid -d' ' -f)
 
 kill -9 $main_pid
 
-if [ -z "$child_pids" ]; then
-    echo "No ${process_name} processes are running."
-else
-    echo "Killing ${process_name} processes:"
-    echo -e "\t ${child_pids}"
-    
-    for pid in $child_pids; do
-        kill $pid
-        if [ $? -eq 0 ]; then
-            echo -e "\t\tProcess ${process_name} with PID ${pid} has been killed."
-        else
-            echo -e "\t\tFailed to kill process ${process_name} with PID ${pid}."
-        fi
-    done
-fi
+echo -e "\t\tProcess ${process_name} with PID ${main_pid} has been killed."
 
 rm $PID_FILE
